abc64e6cb4593105ac525fe8ed7c20226d04d44a
Ethan 『 Eltymme 』 <eltymme@outlook.fr>
2025-12-04 23:43:03 +0100

    [ADD] hexcolor class

---
+++ Common/HexColor.cs
@@ -0,0 +1,14 @@
+namespace Eltymme.FireflyMod.Common;
+
+public abstract class HexColor
+{
+    public const string Black = "#000000";
+    public const string White = "#FFFFFF";
+    public const string Red = "#FF0000";
+    public const string Green = "#00FF00";
+    public const string Blue = "#0000FF";
+    public const string Yellow = "#FF00FF";
+    public const string Cyan = "#00FFFF";
+    public const string Magenta = "#FF00FF";
+    public const string Orange = "#FFA500";
+}
